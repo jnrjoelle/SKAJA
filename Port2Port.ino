de5f507de499295cf6ac184b5cf74ff40a9625ba
Atn00 <31675294+Atn00@users.noreply.github.com>
2017-09-26 14:23:48 -0400

    Update Port2Port.ino

--- Port2Port.ino
+++ Port2Port.ino
@@ -18,7 +18,7 @@ void loop() { // run over and over
       mySerial.write(&token);
     }
   }
-  else {
+  else {//if token is filled with information check for data
     int begdest = token.indexOf("/");
     int enddest = token.indexOf("-");//seperates the destination from the rest of the string
     if (token.substring(begdest + 1, enddest).equals(myaddress)) {//if my address is the one being sent to accept message
